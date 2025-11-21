701a0ab5e2fb624ea33245b0370908a6b01977ef
hehe <jitxuan2021@gmail.com>
2025-11-21 22:02:45 +0800

    fixing bugs

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,7 @@ function App() {
   return (
     <>
       <WelcomeScreen />
-      <div className="bg-glow bg-glow-1"></div>
-      <div className="bg-glow bg-glow-2"></div>
+      <div className="animated-bg"></div>
       <Navbar />
       <Hero />
       <About />
